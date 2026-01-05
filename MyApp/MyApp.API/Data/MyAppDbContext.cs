5f0b226c1babcf4e66e6a2d136e40eac98904990
Shubhankar Banerjee <shubhankar.banerjee@csharp.com>
2026-01-05 15:41:55 +0530

    Initiated MyApp and added Db Context and connection string all using EFCore

---
+++ MyApp/MyApp.API/Data/MyAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyApp.API.Data
+{
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
