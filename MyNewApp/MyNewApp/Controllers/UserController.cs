c9fa267f1bfc678044eb6bb42ff337d189c97045
Shubhankar Banerjee <shubhankar.banerjee@csharp.com>
2026-01-10 21:50:49 +0530

    added a new controller for user

---
+++ MyNewApp/MyNewApp/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyNewApp.Data;
+
+namespace MyNewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(MyNewAppDbContext myNewDbContext) : ControllerBase
+    {
+        [HttpPost]
+        [Route("SignUp")]
+        public Task<IActionResult> SignUp([FromBody])
+        {
+
+        }
+    }
+}
